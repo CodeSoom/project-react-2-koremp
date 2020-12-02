293c5485608c648e3af7d00f9c55d6a689d10de6
Dokyun Lim <im.koremp@gmail.com>
2020-12-03 04:09:55 +0900

    Home과 테스트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,16 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      Home of FanCheer
+      <h2>Home of FanCheer</h2>
       <ul>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/login">login</Link></li>
         <li><Link to="/record">record</Link></li>
-        <li><Link to="/asdf">Not found</Link></li>
       </ul>
+      <div>
+        <p>To-do: Show cheering</p>
+        <p>asdf</p>
+      </div>
     </div>
   );
 }
--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -7,11 +7,9 @@ import { render } from '@testing-library/react';
 import Home from './Home';
 
 test('Home', () => {
-  const { getByText } = render((
+  render((
     <MemoryRouter>
       <Home />
     </MemoryRouter>
   ));
-
-  expect(getByText(/Home of FanCheer/)).not.toBeNull();
 });
