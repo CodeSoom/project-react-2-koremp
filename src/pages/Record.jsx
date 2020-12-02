303ebe966942bdeb44c840ea29cb54f3fe70cd40
Dokyun Lim <im.koremp@gmail.com>
2020-12-03 01:29:34 +0900

    ChooseRecordType에 맞게 Record 파일 변경

--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
+import ChooseRecordType from './ChooseRecordType';
 import RecordMic from './RecordMic';
 import RecordWebcam from './RecordWebcam';
 
 export default function Record({ match }) {
   return (
     <div>
-      <h1>Record Page</h1>
-      <ul>
-        <li><Link to={`${match.path}/mic`}>mic</Link></li>
-        <li><Link to={`${match.path}/webcam`}>webcam</Link></li>
-      </ul>
+      <h1>Record</h1>
+      <Route exact path={match.path} component={ChooseRecordType} />
       <Route path={`${match.path}/mic`} component={RecordMic} />
       <Route path={`${match.path}/webcam`} component={RecordWebcam} />
     </div>
