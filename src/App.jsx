0b1f55247a1b68569c139575356c1c7316825102
Dokyun Lim <im.koremp@gmail.com>
2020-12-03 04:09:47 +0900

    App과 테스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
-import Record from './pages/Record';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import ChooseRecordType from './pages/ChooseRecordType';
+import RecordMic from './pages/RecordMic';
+import RecordWebcam from './pages/RecordWebcam';
 
 function Cheer() {
   return (
@@ -16,13 +18,22 @@ function Cheer() {
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/login" component={Login} />
-      <Route path="/cheer" component={Cheer} />
-      <Route exact path="/record" component={Record} />
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <header>
+        <h1>
+          <Link to="/">Header</Link>
+        </h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+        <Route path="/cheer" component={Cheer} />
+        <Route exact path="/record" component={ChooseRecordType} />
+        <Route path="record/mic" component={RecordMic} />
+        <Route path="record/webcam" component={RecordWebcam} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,10 +6,44 @@ import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  ));
+describe('App', () => {
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('Home of FanCheer');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('FanCheer is online cheering service.');
+    });
+  });
+
+  context('with path /record', () => {
+    it('renders the record page', () => {
+      const { container } = renderApp({ path: '/record' });
+
+      expect(container).toHaveTextContent('Record');
+    });
+  });
+
+  context('with path /xxx', () => {
+    it('renders the not found page', () => {
+      const { container } = renderApp({ path: '/xxx' });
+
+      expect(container).toHaveTextContent('404 Not Found');
+    });
+  });
 });
