a14d2611154e831efde2cb227b55ff09274a2f0a
Dokyun Lim <im.koremp@gmail.com>
2020-12-03 10:48:09 +0900

    Fixtures 추가
    Agency, Artist, Songs, Song, AccessToken

---
+++ fixtures/songs.js
@@ -0,0 +1,10 @@
+const songs = [
+  {
+    id: 1, artist: 'BTOB', agency: 'CUBE', name: '그리워하다',
+  },
+  {
+    id: 2, artist: 'TAEMIN', agency: 'SM', name: 'WANT',
+  },
+];
+
+export default songs;
