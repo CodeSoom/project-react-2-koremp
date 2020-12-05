b07d95ba76c3e106436584199499ebc6ad47d0be
Dokyun Lim <im.koremp@gmail.com>
2020-12-06 03:48:44 +0900

    Song page 변경

--- src/page/Song.jsx
+++ src/page/Song.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-export default function Song() {
+import { Link } from 'react-router-dom';
+
+import SongInfo from '../container/SongInfo';
+import AlbumInfo from '../container/AlbumInfo';
+import ArtistInfo from '../container/ArtistInfo';
+
+export default function Song({ match }) {
+  const { id } = match.params;
   return (
-    <div>asdf</div>
+    <div>
+      <SongInfo />
+      <AlbumInfo />
+      <ArtistInfo />
+      <hr />
+      <Link to={`/song/${id}/record`}>to RecordPage</Link>
+    </div>
   );
 }
