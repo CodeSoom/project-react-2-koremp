84502cf5225901a0b89987be82855b488a04072f
Dokyun Lim <im.koremp@gmail.com>
2020-12-09 04:02:09 +0900

    update src/index.jsx and dist/main.js

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter basename="https://codesoom.github.io/project-react-2-koremp/">
+      <BrowserRouter basename="/project-react-2-koremp/">
         <App />
       </BrowserRouter>
     </Provider>
