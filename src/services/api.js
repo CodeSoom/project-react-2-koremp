31def682cb3309f23a38ffbf3894510ea01ba2e9
Dokyun Lim <im.koremp@gmail.com>
2020-12-10 00:50:59 +0900

    api.js 변경
    url에서 fetch하는 대신 fixtures에서 데이터를 가져오도록 수정

--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,65 @@
+import agencies from '../../fixtures/agencies';
+import artists from '../../fixtures/artists';
+import songs from '../../fixtures/songs';
+
+// for async func
+
+function resolveAgencies() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(agencies);
+    }, 100);
+  });
+}
+
+function resolveArtists({ agencyName }) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(artists.filter((artist) => artist.agency === agencyName));
+    }, 100);
+  });
+}
+
+function resolveSongs({ artistName }) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(songs.filter((song) => song.filter(song.artist === artistName)));
+    }, 100);
+  });
+}
+
 export async function fetchAgencies() {
-  const url = 'http://localhost:3000/agency';
-  const response = await fetch(url);
-  const data = await response.json();
+  // const url = 'http://localhost:3000/agency';
+  // const response = await fetch(url);
+  // const data = await response.json();
+  const data = await resolveAgencies();
   return data;
 }
 
 export async function fetchArtists({ agencyName }) {
-  const url = 'http://localhost:3000/artist'
-    + `?agency=${agencyName}`;
-  const response = await fetch(url);
-  const data = await response.json();
+  // const url = 'http://localhost:3000/artist'
+  //   + `?agency=${agencyName}`;
+  // const response = await fetch(url);
+  // const data = await response.json();
+  const data = await resolveArtists({ agencyName });
   return data;
 }
 
 export async function fetchSongs({ artistName }) {
-  const url = 'http://localhost:3000/song'
-    + `?artist=${artistName}`;
-  const response = await fetch(url);
-  const data = await response.json();
+  // const url = 'http://localhost:3000/song'
+  //   + `?artist=${artistName}`;
+  // const response = await fetch(url);
+  // const data = await response.json();
+  const data = await resolveSongs({ artistName });
   return data;
 }
 
 export async function fetchSong({ songId }) {
-  const url = 'http://localhost:3000/song'
-    + `?id=${songId}`;
-  const response = await fetch(url);
-  const data = await response.json();
+  // const url = 'http://localhost:3000/song'
+  //   + `?id=${songId}`;
+  // const response = await fetch(url);
+  // const data = await response.json();
+  const data = songs.filter((song) => song.id === songId);
   return data;
 }
 
