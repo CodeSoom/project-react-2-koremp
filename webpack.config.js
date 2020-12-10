80b1e1265cf48d5b3bc10575a1403fd1e8065a5c
Dokyun Lim <im.koremp@gmail.com>
2020-12-10 22:35:58 +0900

    webpack config에 html-webpack-plugin 관련 내용 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
