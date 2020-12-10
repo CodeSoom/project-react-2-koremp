72ba61b0785337fd37183058a7983a459715f5a0
Dokyun Lim <im.koremp@gmail.com>
2020-12-11 08:38:29 +0900

    fixtures의 song 변경

--- fixtures/song.js
+++ fixtures/song.js
@@ -1,12 +1,5 @@
-const song = [
-  {
-    id: 1,
-    artist: 'BTOB',
-    agency: 'CUBE',
-    name: '그리워하다',
-    mvUrl: 'https://youtube.com/embed/wDkjWSt3HOM',
-    cheerUrl: 'https://youtube.com/embed/NrdFqYGfCys',
-  },
-];
+import songs from './songs';
+
+const song = songs.filter((_) => _.id === 1);
 
 export default song;
