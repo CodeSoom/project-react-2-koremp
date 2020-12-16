47deaf80ede60a56d52b7840a8a831e1c30a39fd
Dokyun Lim <im.koremp@gmail.com>
2020-12-17 00:31:00 +0900

    SongList page 디자인 추가

--- src/page/SongList.jsx
+++ src/page/SongList.jsx
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import Agencies from '../container/Agencies';
 import Artists from '../container/Artists';
 import Songs from '../container/Songs';
@@ -11,6 +13,18 @@ import Songs from '../container/Songs';
 import {
   loadAgencies,
 } from '../slice';
+
+const Container = styled.div({
+  display: 'flex',
+  flex: '1',
+});
+
+const DivideContainer = styled.div({
+  flex: '1',
+  '& p': {
+    fontWeight: 'bold',
+  },
+});
 
 export default function SongList() {
   const history = useHistory();
@@ -28,13 +42,22 @@ export default function SongList() {
 
   return (
     <div>
-      <h2>Choose Agency, Artist, and Song.</h2>
-      <p>Agency</p>
-      <Agencies />
-      <p>Artist</p>
-      <Artists />
-      <p>Song</p>
-      <Songs onClickSong={handleClickSong} />
+      <h2>소속사, 가수, 노래를 차례대로 선택하세요.</h2>
+      <Container>
+        <DivideContainer>
+          <p>소속사</p>
+          <Agencies />
+        </DivideContainer>
+        <DivideContainer>
+          <p>가수</p>
+          <Artists />
+        </DivideContainer>
+        <DivideContainer>
+          <p>노래</p>
+          <Songs onClickSong={handleClickSong} />
+        </DivideContainer>
+      </Container>
     </div>
+
   );
 }
