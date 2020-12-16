c106455bd26123d092e6b7951ffbf53441796f01
Dokyun Lim <im.koremp@gmail.com>
2020-12-17 00:30:50 +0900

    Home page 디자인 추가

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 import { Link } from 'react-router-dom';
+
+const NavigationContainer = styled.div({
+
+});
+
+const List = styled.ul({
+  display: 'flex',
+  margin: 0,
+  padding: 0,
+  listStyle: 'none',
+});
+
+const Item = styled.li({
+  marginRight: '1em',
+  '& a': {
+    color: '#333',
+    textDecoration: 'none',
+    '&:hover': {
+      fontWeight: 'bold',
+      color: '#000',
+    },
+  },
+});
 
 export default function Home() {
   return (
     <div>
-      <h2>Home of FanCheer</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">login</Link></li>
-        <li><Link to="/song">song</Link></li>
-      </ul>
-      <div>
-        <p>To-do: Show cheering</p>
-        <p>asdf</p>
-      </div>
+      <NavigationContainer>
+        <List>
+          <Item><Link to="/about">About</Link></Item>
+          <Item><Link to="/login">login</Link></Item>
+          <Item><Link to="/song">song</Link></Item>
+        </List>
+      </NavigationContainer>
     </div>
   );
 }
