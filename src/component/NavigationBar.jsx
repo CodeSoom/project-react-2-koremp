15b0c18a1a3737239ec94a12ac51649b5cd5a33b
Dokyun Lim <im.koremp@gmail.com>
2020-12-22 13:50:58 +0900

    NavigationBar 추가

---
+++ src/component/NavigationBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+export default function NavigationBar() {
+  return (
+    <div>
+      <p>logo</p>
+      <p>user</p>
+      <p>CheerVideo</p>
+    </div>
+  );
+}
