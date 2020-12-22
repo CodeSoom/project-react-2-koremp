88bb88fd393a2c8c5c883af84958c3158768da7a
Dokyun Lim <im.koremp@gmail.com>
2020-12-22 22:59:31 +0900

    TrendingList component 추가

---
+++ src/component/home/TrendingList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+export default function TrendingList() {
+  return (
+    <div>
+      Trending Song List
+    </div>
+  );
+}
